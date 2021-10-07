b5cfedefcffa110979fcb15e6318e9d0df824454
Dmitriy <57622791+dimitriynoway@users.noreply.github.com>
2021-10-07 17:38:22 +0300

    Update socket.use.ts
    Changed socket.auth.token check

--- server/src/sockets/socket.use.ts
+++ server/src/sockets/socket.use.ts
@@ -14,27 +14,31 @@ interface Next {
 
 const socketUse = async (socket: Socket, next: Next) => {
 	try {
-		if (socket.handshake.auth && socket.handshake.auth.token) {
-			const decoded = <JwtPayload>jwt.verify(
-				socket.handshake.auth.token,
-				process.env.SECRET_KEY as string,
+		if (!socket.handshake.auth && !socket.handshake.auth.token){
+			socket.emit('error', 'Token not passed');
+			socket.disconnect()
+		}
+		
+		const decoded = <JwtPayload>jwt.verify(
+			socket.handshake.auth.token,
+			process.env.SECRET_KEY as string,
 			);
 			// decoded = {id, username, role}
 			// take from db actual data and check if user is banned
-			const decodedId = decoded.id;
-			const findedUser = await checkUserInDB(decodedId);
+		const decodedId = decoded.id;
+		const findedUser = await checkUserInDB(decodedId);
 
-			if (!findedUser) {
-				throw new Error('User not found');
-			}
-			if (findedUser.banned) {
-				// socket.disconnect(true)
-				throw new Error('You are banned');
-			}
-			setCurrentUserStatus(findedUser, socket);
-			checkDoubleConnection(socket);
-			next();
+		if (!findedUser) {
+			throw new Error('User not found');
 		}
+		if (findedUser.banned) {
+				// socket.disconnect(true)
+			throw new Error('You are banned');
+		}
+		setCurrentUserStatus(findedUser, socket);
+		checkDoubleConnection(socket);
+		next();
+		
 	} catch (err) {
 		socket.emit('error', err.message);
 		socket.disconnect()
